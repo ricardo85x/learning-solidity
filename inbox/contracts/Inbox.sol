854dde769c619e5a511ccea826a0df5b404196c0
ricardo <ricardo.egss@gmail.com>
2019-04-05 18:33:49 -0300

    ow my

---
+++ inbox/contracts/Inbox.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5;
+
+// 2 
+contract Inbox {
+    string public message;
+    
+    constructor (string memory initialMessage) public {
+        message = initialMessage;
+    }
+    
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+    
+    function doMath(int  a, int  b) public {
+        a+b;
+        b-a;
+        a*b;
+        a==0;
+        
+    }
+
+}
