ffb147b16a161792c3ed57463aebd5f4bff32699
Ricardo <ricardo85x@gmail.com>
2019-04-06 13:28:50 -0300

    Auula 15

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,31 +3,22 @@ const ganache = require('ganache-cli');
 const  Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-
-class Car {
-    park() {
-        return 'stopped';
-    }
-
-    drive() {
-        return 'vroom';
-    }
-}
-
-
-let car;
-
 beforeEach(() => {
-    console.log('primeiro')
-    car = new Car();
+    // Get a list of all accounts
+    
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
 
-})
-describe('Carro vulgo Car', () => {
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped');
-    });
 
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom');
-    });
+    // use one of these accounts to deloy the ccontract
+
+});
+
+
+describe('Le Inbox', () => {
+    it('deploy a contract', () => {
+
+    })
 })
