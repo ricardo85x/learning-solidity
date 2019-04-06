1dd79a3786893e8cd120e16ccb9bf978b9dfbbf9
ricardo <ricardo.egss@gmail.com>
2019-04-06 11:25:16 -0300

    aula 13 TEST

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,3 +2,32 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const  Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom';
+    }
+}
+
+
+let car;
+
+beforeEach(() => {
+    console.log('primeiro')
+    car = new Car();
+
+})
+describe('Carro vulgo Car', () => {
+    it('can park', () => {
+        assert.equal(car.park(), 'stopped');
+    });
+
+    it('can drive', () => {
+        assert.equal(car.drive(), 'vroom');
+    });
+})
