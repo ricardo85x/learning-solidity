10c42d51a35b9cf23dfd1f4e08fd7bc4bfc3e4db
ricardo <ricardo85x@gmail.com>
2019-04-18 20:19:25 -0300

    aula 14

--- 03_Loteria/contracts/loteria.sol
+++ 03_Loteria/contracts/loteria.sol
@@ -1,9 +1,32 @@
 pragma solidity ^0.5;
 
 contract Loteria {
+    
     address public gerente;
-
+    
+    address payable[] public jogadores;
+    
     constructor () public {
+        gerente = msg.sender;
         
     }
+    
+    function enter() public payable {
+        require(msg.value > .0001 ether);
+        jogadores.push(msg.sender);
+    }
+    
+    
+    function random() private view returns (uint ) {
+        
+        return uint( keccak256(abi.encodePacked(  block.difficulty, now, jogadores))  );
+    }
+    
+    
+    function escoolherGanhador() public  {
+        uint index = random() % jogadores.length;
+        jogadores[index].transfer(address(this).balance);
+        
+    }
+    
 }
